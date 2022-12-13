b2e2c5a282ed38643edd802881bb359eba9c4fb9
Hetvipatel10 <patelhetvi20@gmail.com>
2022-12-13 10:55:08 +0000

    First

---
+++ src/Americanflag.java
@@ -0,0 +1,23 @@
+package Homework;
+
+public class Americanflag {
+    public static void main(String[] args) {
+
+        System.out.println("***** ========================");
+        System.out.println("***** ========================");
+        System.out.println("****** =======================");
+        System.out.println("*****  =======================");
+        System.out.println("****** ========================");
+        System.out.println("*****  ========================");
+        System.out.println("****** ========================");
+        System.out.println("*****  ========================");
+        System.out.println("****** =========================");
+        System.out.println("================================");
+        System.out.println("================================");
+        System.out.println("================================");
+        System.out.println("=================================");
+        System.out.println("=================================");
+
+
+    }
+}
